57de9df440ddb0cad60471483b5af6af7c529464
User24 <user24@user24de-imac.local>
2019-03-19 18:12:37 +0800

    Initial Commit

---
+++ Auto_chessEbook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto_chessEbook
+//
+//  Created by User24 on 2019/3/19.
+//  Copyright © 2019 User24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
